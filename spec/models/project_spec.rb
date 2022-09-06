c0445fe91954c20ab86289b103f2f25e997f998b
Lucas Colwell <colwellc37@gmail.com>
2022-09-05 20:40:46 -0600

    Test: model test theme method

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,4 +12,20 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  describe 'class methods' do
+    before :each do
+      @recycled_material_challenge = Challenge.create!(theme: "Recycled Material", project_budget: 1000)
+      @furniture_challenge = Challenge.create!(theme: "Apartment Furnishings", project_budget: 1000)
+
+      @upholstery_tux = @furniture_challenge.projects.create!(name: "Upholstery Tuxedo", material: "Couch")
+      @lit_fit = @furniture_challenge.projects.create!(name: "Litfit", material: "Lamp")
+    end
+
+    describe 'theme' do
+      it "returns challenge theme" do
+        expect(@lit_fit.theme).to eq("Apartment Furnishings")
+      end
+    end
+  end
 end
